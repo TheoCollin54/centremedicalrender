f091c597a120023237f3060e01184482721875a9
TheoCollin54 <theo.collin054@gmail.com>
2025-06-04 13:38:26 +0200

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -11,6 +11,3 @@ RUN chown -R www-data:www-data /var/www/html
 
 # Port exposé
 EXPOSE 80
-
-RUN apt-get update && apt-get install -y libpq-dev \
-    && docker-php-ext-install pdo pdo_pgsql
--- add_rdv.php
+++ add_rdv.php
@@ -5,7 +5,7 @@ $dbname = 'centre-medical';
 $username_db = 'root';
 $password_db = '';
 try {
-    $pdo = new PDO("pgsql:host=$host;dbname=$dbname;charset=utf8", $username_db, $password_db);
+    $pdo = new PDO("pgsql:host=$host;dbname=$dbname", $username_db, $password_db);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Erreur de connexion : " . $e->getMessage());
