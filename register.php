e1ec7b28c63408ed9eef190a64be7acb1ecd6f3f
TheoCollin54 <theo.collin054@gmail.com>
2025-06-04 14:05:07 +0200

    Add files via upload

--- register.php
+++ register.php
@@ -7,7 +7,7 @@ $user = getenv('DB_USER');
 $password = getenv('DB_PASS');
 
 try {
-    $pdo = new PDO("pgsql:host=$host;dbname=$dbname", $username_db, $password_db);
+    $pdo = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Erreur de connexion : " . $e->getMessage());
